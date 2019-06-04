028876532e93d7a48fde4607d780ce1b2dc31806
Jaesun Park <zzangjames75@gmail.com>
2019-06-05 03:42:57 +0900

    frontend connecting

--- public/javascript/my-js.js
+++ public/javascript/my-js.js
@@ -2,7 +2,7 @@ function mySubmit(){
     var pwd = document.getElementsByName('pwd')[0];
     var conf = document.getElementsByName('pwdconf')[0];
 
-    if(PerfWidgetExternal.value != conf.value){
+    if(pwd.value != conf.value){
         conf.style.borderColor = "#FF322D";
         return false;
     } else{
